118a65d7beb92cebbf05590edddb33e631e41510
carinalsilverio <carinalsilverio@gmail.com>
2023-01-18 21:13:38 -0300

    Inital commit

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = '5cb90b213eef9fc6452911dc196b3643';
+let img_url = 'https://image.tmdb.org/t/p/w500/';
+let genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?';
+let movie_genres_http = 'https://api.themoviedb.org/3/discover/movie?';
